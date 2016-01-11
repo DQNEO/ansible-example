5def705055b3ac4c509a5444a11683243e5f90a1
DQNEO <dqneoo@gmail.com>
2016-01-11 22:48:00 +0900

    save mysql tmp password

---
+++ roles/mysql/files/save-mysql-tmp-password.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu
+log_file=/var/log/mysqld.log
+tmp_file=/root/.tmp.my.cnf
+
+if [[ -e $tmp_file ]]; then
+    exit 0
+fi
+
+tmp_pass=$(sudo grep 'password is generated' $log_file | awk -F'root@localhost: ' '{print $2}')
+
+cat > $tmp_file <<EOF
+[client]
+user=root
+password=$tmp_pass
+EOF
+
